3b4826250537c9dea5445d16c1903352337e824b
Addy Hebou <addyhebou@Addys-MacBook-Pro.local>
2020-10-22 15:10:44 -0400

    feat(app, login, spotify): account authenticates, then stores token on login

--- src/App.js
+++ src/App.js
@@ -1,11 +1,45 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import Login from "./components/Login";
+import { getTokenFromUrl } from './spotify';
+import SpotifyWebApi from "spotify-web-api-js";
+
+const spotify = new SpotifyWebApi();
 
 function App() {
+  // State is short term memory for handling variables
+  // Disappears when page refreshes
+  const [token, setToken] = useState(null)
+
+  // Run code based on a given condition
+  useEffect(() => {
+    const hash = getTokenFromUrl();
+    window.location.hash = "";
+    const _token = hash.access_token;
+
+    if(_token){
+      setToken(_token) // stores token in State
+
+      spotify.setAccessToken(_token);
+
+      spotify.getMe().then(user => {
+        console.log("💪🏿", user);
+      })
+    }
+
+    console.log('I HAVE A TOKEN: 🔥', token);
+  }, []);
+
   return (
     <div className="app">
-      <Login />
+      {
+        token ? ( // If there is a token
+          <h1>I am logged in</h1>
+          // <Player />
+        ) : ( // Else if there is no token
+          <Login />
+        )
+      }
     </div>
   );
 }
--- src/spotify.js
+++ src/spotify.js
@@ -1,7 +1,8 @@
 
 
+
 export const authEndpoint = 
-"https://account.spotify.com/authorize";
+"https://accounts.spotify.com/authorize";
 
 const redirectUri = "http://localhost:3000/";
 const clientId = "c8b5d20bd8bf4e64b2e4bb2c52194a08";
@@ -14,3 +15,19 @@ const scopes = [
     "user-top-read",
     "user-modify-playback-state",
 ];
+
+export const getTokenFromUrl = () => {
+    return window.location.hash
+        .substring(1)
+        .split("&")
+        .reduce((initial, item) => {
+            // #accessToken=mysupersecretkey&name=sonny
+            let parts = item.split('=');
+            initial[parts[0]] = 
+            decodeURIComponent(parts[1]);
+
+            return initial;
+        }, {}); 
+}
+
+export const loginUrl = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join("%20")}&response_type=token&show_dialog=true`;
