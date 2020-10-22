f145a1805bf23d7ed6c1b1d071ccd13faacc84fb
Addy Hebou <addyhebou@Addys-MacBook-Pro.local>
2020-10-22 02:49:05 -0400

    feat(login, app): added logo and button

--- src/components/Login.js
+++ src/components/Login.js
@@ -4,6 +4,7 @@ import '../Login.css'
 function Login() {
     return (
         <div className="login">
+            {/*  */}
             <h1>I am Login Page</h1>
             <img src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg" alt = "Spotify Logo"/>
             <button>LOGIN WITH SPOTIFY</button>
