c93a530a07cfb3185ccf93ec743fe7eabd8e5724
Addy Hebou <addyhebou@Addys-MacBook-Pro.local>
2020-10-28 02:16:58 -0400

    feat(firebase.js): added firebase config info

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCYRTRIZo288vAMwFjG1ICxygZoussr7A0",
+    authDomain: "new-spotify-clone.firebaseapp.com",
+    databaseURL: "https://new-spotify-clone.firebaseio.com",
+    projectId: "new-spotify-clone",
+    storageBucket: "new-spotify-clone.appspot.com",
+    messagingSenderId: "639929147593",
+    appId: "1:639929147593:web:377367c800327b243f2d18"
+  };
+ 
