de751d68760c23a3a872f052b8a658493098d7f5
Addy Hebou <addyhebou@Addys-MacBook-Pro.local>
2020-11-17 13:37:38 -0500

    feat(trending.js):added posts on the trending page

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -1,12 +1,29 @@
 import React from 'react';
+import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import ChatBubbleOutlineIcon from '@material-ui/icons/ChatBubbleOutline';
+import ChatBubbleIcon from '@material-ui/icons/ChatBubble';
+import '../Post.css';
 
-function Post() {
+function Post({ profile = "", name, date = "Jun 17", mediaUrl, text }: { profile: string, name: string, date: string, mediaUrl: string, text: string }) {
+    let socialname = name.split(" ").join("").toLowerCase();
     return (
         <div className="holder">
-            <div className="topline"></div>
-            <div className="text"></div>
-            <div className="media"></div>
-            <div className="bottomline"></div>
+            <div className="topline">
+                <img src = {profile} alt = "Profile Picture"/>
+                <strong>{name}</strong>
+                <p>@{socialname} | {date}</p>
+            </div>
+            <div className="text">
+                <p>{text}</p>
+            </div>
+            <div className="media">
+                <img src = {mediaUrl} alt = "Media Link"/>
+            </div>
+            <div className="bottomline">
+                <ChatBubbleOutlineIcon /><p>17</p>
+                <FavoriteBorderIcon /><p>8</p>
+            </div>
         </div>
     )
 }
--- src/components/TrendingBody.js
+++ src/components/TrendingBody.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from './Header'
-import '../Body.css';
+import Post from './Post.tsx'
+import '../TrendingBody.css';
 import { useDataLayerValue } from '../DataLayer';
 import PlayCircleFilledIcon from '@material-ui/icons/PlayCircleFilled';
 import FavoriteIcon from '@material-ui/icons/Favorite';
@@ -11,33 +12,46 @@ import SongRow from "../components/SongRow";
 
 function TrendingBody({ spotify }) {
     const [{ user, discover_weekly }, dispatch] = useDataLayerValue();
+    let text1 = 
+    `
+        Cole and Kendrick literally hit the streets. Why do they need to tweet? 
+        They’ve literally always used their platform to shed light on these kinds of issues. 
+        Let’s build each other up, there’s no need for this kind of infighting. 
+        SMH #JCole #KendrickLamar #NoName #SnowOnThaBluff
+
+    `
+    let profile1 = "https://pbs.twimg.com/profile_images/1244423745860784128/GuH12aEt_400x400.jpg"
+    let text2 = 
+    `
+    A lot of activist need to really listen to the lyrics of this new J. Cole song
+
+    Unfortunately a lot of you "woke" niggas have superiority complex
+    
+    #SnowOnThaBluff
+    `
+    let profile2 = "https://pbs.twimg.com/profile_images/1244423745860784128/GuH12aEt_400x400.jpg"
+    let text3 = ` @noname drops #Song33 a response to @JCole #SnowOnThaBluff`
+    let profile3 = "https://pbs.twimg.com/profile_images/1244423745860784128/GuH12aEt_400x400.jpg"
+    let text4 = `Morning.  I stand behind every word of the song that dropped last night #SnowOnThaBluff`
+    let profile4 = "https://pbs.twimg.com/profile_images/1244423745860784128/GuH12aEt_400x400.jpg"
 
     return (
         <div className="body">
             <Header spotify={spotify}/>
             <div className = "body_info">
-                <img src = {discover_weekly?.images[0].url} /* {user?.images[0]?.url} */ alt = ""/>
                 <div className = "body_infoText">
-                    <strong>Trending</strong>
                     <h2>Trending</h2>
                     <p><strong>J. Cole </strong>Snow On Tha Bluff</p>
                     <p>{discover_weekly?.description}</p>
                 </div>
             </div>
-
-            <div className = "body_songs">
-                <div className = "body_icons">
-                    <PlayCircleFilledIcon className = "body_shuffle"/>
-                    <FavoriteIcon fontSize="large"/>
-                    <MoreHorizIcon />
-                </div>
-
-                {/* List of songs */}
-                {discover_weekly?.tracks.items.map((item) =>(
-                    <SongRow track = {item.track} />
-                ))}
-
+            <div className = "postPanel">
+                <Post profile = {profile1} name = "Fred Dalger" text = {text1}/>
+                <Post profile = {profile2} name = "D. Dames" text = {text2}/>
+                <Post profile = {profile3} name = "Complex" text = {text3}/>
+                <Post profile = {profile4} name = "J Cole" text = {text4}/>
             </div>
+            
         </div>
     );
 }
