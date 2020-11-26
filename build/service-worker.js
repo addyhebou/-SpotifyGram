44baa6ec55044ea05cfb611a4ae0c4eb2b978f4f
Addy Hebou <addyhebou@Addys-MacBook-Pro.local>
2020-11-26 00:03:02 -0500

    feat(deploy on react app)

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d362c2d5e68fce757c82a2ea60658690.js"
+  "/-SpotifyGram/precache-manifest.8baa8e1607388ebbf984b08c54eed35a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/-SpotifyGram/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
