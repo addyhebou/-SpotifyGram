9a8da2e1b9ae3bcce21151b0018087b0ae756c0d
Addy Hebou <addyhebou@Addys-MacBook-Pro.local>
2020-11-26 02:47:40 -0500

    feat(deploy): add to heroku from successful past commit

---
+++ build/precache-manifest.b2eb78e40c6dd1841ea2406587d5bf47.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "faaa85058cd7a5046289f773f7459c46",
+    "url": "/index.html"
+  },
+  {
+    "revision": "03ab3b921b34b71abe8a",
+    "url": "/static/css/main.40acb34b.chunk.css"
+  },
+  {
+    "revision": "328b7a51252e5c360c58",
+    "url": "/static/js/2.b5d5dcee.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/static/js/2.b5d5dcee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03ab3b921b34b71abe8a",
+    "url": "/static/js/main.27937e5d.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
