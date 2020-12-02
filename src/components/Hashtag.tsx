3532e046f2cdf57f22f7fd4143e604837ce6b7ff
Addy Hebou <addyhebou@Addys-MacBook-Pro.local>
2020-12-02 02:21:46 -0500

    fix(deploy): chnaged tag data type to string | '' (empty string)

--- src/components/Hashtag.tsx
+++ src/components/Hashtag.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import '../css/Hashtag.css';
 
 
-function Hashtag({tag} : {tag: string | null}) {
+function Hashtag({tag} : {tag: string | ""}) {
     return (
         <div className = "hashtag">
             <h3>#{tag}</h3>
